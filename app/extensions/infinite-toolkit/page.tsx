713adc8c671cb56cd7cf69cf2729f825a9931dc5
MechashadowX <mechashadowx@gmail.com>
2024-07-28 10:29:53 +0300

    fix

--- app/extensions/infinite-toolkit/page.tsx
+++ app/extensions/infinite-toolkit/page.tsx
@@ -1,7 +1,7 @@
 export default function InfiniteToolkit() {
     return (
-        <div className="m-16 flex flex-col items-center gap-4" >
-            <h1 className="text-4xl font-bold" >Privacy Policy</h1>
+        <div className="m-16 flex flex-col items-center gap-4">
+            <h1 className="text-4xl font-bold">Privacy Policy</h1>
             <p>Last updated: July 28, 2024</p>
             <p>
                 This Privacy Policy describes Our policies and procedures on the
@@ -145,11 +145,11 @@ export default function InfiniteToolkit() {
             <h4>Usage Data</h4>
             <p>Usage Data is collected automatically when using the Service.</p>
             <p>
-                Usage Data may include information such as Your Device's
-                Internet Protocol address (e.g. IP address), browser type,
-                browser version, the pages of our Service that You visit, the
-                time and date of Your visit, the time spent on those pages,
-                unique device identifiers and other diagnostic data.
+                Usage Data may include information such as Your Devices Internet
+                Protocol address (e.g. IP address), browser type, browser
+                version, the pages of our Service that You visit, the time and
+                date of Your visit, the time spent on those pages, unique device
+                identifiers and other diagnostic data.
             </p>
             <p>
                 When You access the Service by or through a mobile device, We
@@ -294,7 +294,7 @@ export default function InfiniteToolkit() {
                         <strong>To contact You:</strong> To contact You by
                         email, telephone calls, SMS, or other equivalent forms
                         of electronic communication, such as a mobile
-                        application's push notifications regarding updates or
+                        applications push notifications regarding updates or
                         informative communications related to the
                         functionalities, products or contracted services,
                         including the security updates, when necessary or
@@ -401,7 +401,7 @@ export default function InfiniteToolkit() {
             <h3>Transfer of Your Personal Data</h3>
             <p>
                 Your information, including Personal Data, is processed at the
-                Company's operating offices and in any other places where the
+                Companys operating offices and in any other places where the
                 parties involved in the processing are located. It means that
                 this information may be transferred to — and maintained on —
                 computers located outside of Your state, province, country or
@@ -485,7 +485,7 @@ export default function InfiniteToolkit() {
                 use commercially acceptable means to protect Your Personal Data,
                 We cannot guarantee its absolute security.
             </p>
-            <h2>Children's Privacy</h2>
+            <h2>Childrens Privacy</h2>
             <p>
                 Our Service does not address anyone under the age of 13. We do
                 not knowingly collect personally identifiable information from
@@ -499,14 +499,14 @@ export default function InfiniteToolkit() {
             <p>
                 If We need to rely on consent as a legal basis for processing
                 Your information and Your country requires consent from a
-                parent, We may require Your parent's consent before We collect
+                parent, We may require Your parents consent before We collect
                 and use that information.
             </p>
             <h2>Links to Other Websites</h2>
             <p>
                 Our Service may contain links to other websites that are not
                 operated by Us. If You click on a third party link, You will be
-                directed to that third party's site. We strongly advise You to
+                directed to that third partys site. We strongly advise You to
                 review the Privacy Policy of every site You visit.
             </p>
             <p>
