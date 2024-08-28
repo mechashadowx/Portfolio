0112be63a880d1de0cf23a3e7470437c166702a8
MechashadowX <mechashadowx@gmail.com>
2024-08-28 17:12:31 +0300

    Implement fade in on scroll animation

--- app/projects/Projects.tsx
+++ app/projects/Projects.tsx
@@ -15,6 +15,7 @@ import leetcode from "@/public/logos/leetcode.svg";
 import html from "@/public/logos/html.svg";
 import css from "@/public/logos/css.svg";
 import javascript from "@/public/logos/javascript.svg";
+import FadeInSection from "../components/FadeInSection";
 
 const Projects = () => {
     const logoMapping = new Map<string, any>([
@@ -42,67 +43,67 @@ const Projects = () => {
             <div className="flex flex-col items-center gap-12">
                 {projects.map((project: any, index: number) => {
                     return (
-                        <div
-                            key={index}
-                            className="w-full flex flex-col items-center gap-8"
-                        >
-                            <a
-                                href={project.source}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <p className="text-4xl sm:text-5xl text-center font-bold transition-all hover:rotate-3">
-                                    <span className="h-12 sm:h-14 inline-grid">
-                                        <span className="relative col-start-1 row-start-1 bg-gradient-to-r from-green-400 to-blue-400 text-transparent bg-clip-text blur-xl opacity-90 gradient-animation">
-                                            {project.name}
+                        <FadeInSection key={index}>
+                            <div className="w-full flex flex-col items-center gap-8">
+                                <a
+                                    href={project.source}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <p className="text-4xl sm:text-5xl text-center font-bold transition-all hover:rotate-3">
+                                        <span className="h-12 sm:h-14 inline-grid">
+                                            <span className="relative col-start-1 row-start-1 bg-gradient-to-r from-green-400 to-blue-400 text-transparent bg-clip-text blur-xl opacity-90 gradient-animation">
+                                                {project.name}
+                                            </span>
+                                            <span className="relative col-start-1 row-start-1 bg-gradient-to-r from-green-400 to-blue-400 text-transparent bg-clip-text gradient-animation">
+                                                {project.name}
+                                            </span>
                                         </span>
-                                        <span className="relative col-start-1 row-start-1 bg-gradient-to-r from-green-400 to-blue-400 text-transparent bg-clip-text gradient-animation">
-                                            {project.name}
-                                        </span>
-                                    </span>
+                                    </p>
+                                </a>
+                                <p className="text-xl text-center drop-shadow-sm text-gray-400">
+                                    {project.details}
                                 </p>
-                            </a>
-                            <p className="text-xl text-center drop-shadow-sm text-gray-400">
-                                {project.details}
-                            </p>
-                            <div className="flex gap-8 justify-center">
-                                {project.techs.map(
-                                    (tech: string, index: number) => {
-                                        return (
-                                            <a
-                                                key={index}
-                                                href={
-                                                    (techs as any)[tech] ?? ""
-                                                }
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                            >
-                                                <div className="h-6 w-6 drop-shadow-lg flex items-center hover:rotate-12 transition-all duration-150 ease-in-out">
-                                                    <Image
-                                                        priority
-                                                        src={logoMapping.get(
-                                                            tech
-                                                        )}
-                                                        alt=""
-                                                        width={24}
-                                                    />
-                                                </div>
-                                            </a>
-                                        );
-                                    }
-                                )}
+                                <div className="flex gap-8 justify-center">
+                                    {project.techs.map(
+                                        (tech: string, index: number) => {
+                                            return (
+                                                <a
+                                                    key={index}
+                                                    href={
+                                                        (techs as any)[tech] ??
+                                                        ""
+                                                    }
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                >
+                                                    <div className="h-6 w-6 drop-shadow-lg flex items-center hover:rotate-12 transition-all duration-150 ease-in-out">
+                                                        <Image
+                                                            priority
+                                                            src={logoMapping.get(
+                                                                tech
+                                                            )}
+                                                            alt=""
+                                                            width={24}
+                                                        />
+                                                    </div>
+                                                </a>
+                                            );
+                                        }
+                                    )}
+                                </div>
+                                <div className="drop-shadow-2xl">
+                                    <Image
+                                        src={`/Portfolio/projects${project.image}`}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                        className="rounded-2xl hover:scale-110 transition-all duration-150 ease-in-out"
+                                    />
+                                </div>
+                                <div className="divider"></div>
                             </div>
-                            <div className="drop-shadow-2xl">
-                                <Image
-                                    src={`/Portfolio/projects${project.image}`}
-                                    alt=""
-                                    width={500}
-                                    height={500}
-                                    className="rounded-2xl hover:scale-110 transition-all duration-150 ease-in-out"
-                                />
-                            </div>
-                            <div className="divider"></div>
-                        </div>
+                        </FadeInSection>
                     );
                 })}
             </div>
